a1a886f36d0ddcaa3d17dc9aa0e57c27b73e172d
Andrew Yatsura <andrewyazura203@gmail.com>
2023-04-27 22:01:30 +0300

    remove app_root_path

--- training_plan_service/__init__.py
+++ training_plan_service/__init__.py
@@ -2,13 +2,12 @@ from logging.config import dictConfig
 
 from fastapi import FastAPI
 
-from .config import settings
 from .logging import LogConfig
 from .routes import router
 
 
 def build_app() -> FastAPI:
-    app = FastAPI(root_path=settings.app_root_path)
+    app = FastAPI()
     app.include_router(router)
 
     dictConfig(LogConfig().dict())
